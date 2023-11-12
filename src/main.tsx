90773e76cfdd5ec212602693792624419a75e6dc
vitória lopes <heyvitoria.lopes@gmail.com>
2023-11-12 11:42:59 -0300

    new: hostjar setup

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+import Hotjar from '@hotjar/browser';
+
+try {
+  const siteId = Number(import.meta.env.VITE_HOTJAR_ID);
+  const hotjarVersion = Number(import.meta.env.VITE_HOTJAR_VERSION);
+
+  Hotjar.init(siteId, hotjarVersion);
+} catch (e) {
+  console.log('Cannot initialize Hotjar:', e);
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
